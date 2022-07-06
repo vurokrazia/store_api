6ec46824d86417a117539c07e01d1f93ee4c8995
Jesus <jesus.alberto.vk@gmail.com>
2022-07-06 14:39:05 -0500

    Create route enviroment

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  def index
+    render json: { environment: ENV['API_ENV'] }
+  end
   def grettings
     render json: { grettings: I18n.t(:hello) }
   end
