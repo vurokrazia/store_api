9b4bf5b904c05106f67cae1a95964d9e1d681e1b
Jesus <jesus.alberto.vk@gmail.com>
2022-07-06 14:44:12 -0500

    Update locale

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    render json: { environment: ENV['API_ENV'] }
+    render json: { environment: ENV['API_ENV'], locale: I18n.locale }
   end
   def grettings
     render json: { grettings: I18n.t(:hello) }
